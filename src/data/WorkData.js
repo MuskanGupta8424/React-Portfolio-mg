554b89dede86215f4ef8414fb08fb79d49e51d42
zuhouz <muskan@zuhouz.com>
2025-07-07 11:48:02 +0530

    update work section

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -12,6 +12,15 @@ export const Work = [
     },
     {
         id:2,
+        name:"MyWeb3Gift website",
+        description:"A cryptocurrency gifting platform that revolutionizes gifting by combining blockchain technology with the joy of sharing. Offers unique crypto gifting experiences with engaging animations and user-friendly interface.",
+        tags:["React.js","Tailwind CSS","Framer Motion","Blockchain"],
+        
+        demo:"https://myweb3gift.com",
+        github:"https://github.com/MuskanGupta8424/Task1_web3gift"
+    },
+    {
+        id:3,
         name:"The Weirdos Club Clone",
         description:"A Clone of Weirdos Club to build using React JS, with styledComponents and GSAP for smooth scrolling animations.",
         tags:["react","gsap","styledComponents"],
@@ -20,7 +29,7 @@ export const Work = [
         github:"https://github.com/MuskanGupta8424/The-Weirdos-club-clone"
     },
     {
-        id:3,
+            id:4,
         name:"Fashion Wibe Studio",
         description:"A Fashion Studio website build in ReactJS,with styledComponents, locomotive-Scroll for smooth scrolling.Also Gsap and Framer-motion for some custom animations and effects",
         tags:["react-js","gsap","styledComponents"],
@@ -30,7 +39,7 @@ export const Work = [
         
     },
     {
-        id:4,
+        id:5,
         name:"Todo App(React + Redux)",
         description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
         tags:["react","redux","framer-motion"],
@@ -40,7 +49,7 @@ export const Work = [
         
     },
     {
-        id:5,
+        id:6,
         name:"Infos|Connect the brand",
         description:"A cool looking & responsive website build using HTML, CSS, JS, Gsap and other animations.It has really smooth animations.",
         tags:["HTML","CSS","JS","gsap"],
@@ -49,7 +58,7 @@ export const Work = [
         github:"https://github.com/MuskanGupta8424/B15-finalproject"
        
     },{
-        id:6,
+        id:7,
         name:"Neverland-Agency-Clone",
         description:"A clone of Neverland-Agency-Clone to build using Html5, CSS3, JS, Gsap, locomotive-scroll .",
         tags:["HTML5","CSS3","JS","gsap"],
@@ -58,7 +67,7 @@ export const Work = [
         github:"https://github.com/MuskanGUpta8424/Neverland-Agency-Clone"
     },
     {
-        id:7,
+        id:8,
         name:"Flora Responsive Website",
         description:"A reponsive website build using HTML5, CSS3, Javascript, Gsap, LocomotiveScroll, ScrollTrigger.js",
         tags:["Html5","javascript","css","gsap","locomotive-scroll"],
